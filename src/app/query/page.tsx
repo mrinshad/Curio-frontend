fc5afb2e6e37969cdf724d96ca134298d43126d3
MRinshad <rinshadmorayur09@gmail.com>
2024-11-30 01:42:36 +0530

    a small unescaped apostrophe problem solved

--- src/app/query/page.tsx
+++ src/app/query/page.tsx
@@ -19,15 +19,18 @@ export default function Query() {
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleSend();
     }
-  }
+  };
   return (
-    <main className={`${MainStyle.mainContainer} flex items-center justify-center w-full h-screen flex-col`}>
+    <main
+      className={`${MainStyle.mainContainer} flex items-center justify-center w-full h-screen flex-col`}
+    >
       <h1 className={`${styles.typingDemo} text-2xl sm:text-4xl mb-10`}>
-        See What the Crowd's Saying
+        See What the Crowd&apos;s Saying
       </h1>
+
       <div className={`${styles.showAfterTwoSec} flex w-full justify-center`}>
         <input
           type="text"
@@ -36,7 +39,7 @@ export default function Query() {
           onChange={(e) => setTopic(e.target.value)}
           onKeyDown={handleKeyDown}
           className={`w-1/2 lg:w-1/3  xl:w-1/2 py-3 px-3 rounded-full bg-[#161616] text-sm lg:text-lg`}
-          />
+        />
         <div
           className="flex bg-white p-3 rounded-full  ml-2 cursor-pointer hover:opacity-90"
           onClick={handleSend}
